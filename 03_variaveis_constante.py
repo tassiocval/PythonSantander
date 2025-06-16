ef8342acee015c76db1accdecb4a83d74246a5ce
Tassio Cavalcante <tassiocval@yahoo.com>
2025-06-16 18:53:31 -0300

    updates
    tipos de dados, variaveis e constantes, conversao, pritn e input

---
+++ 03_variaveis_constante.py
@@ -0,0 +1,12 @@
+nome = "Tassio"
+idade = 30
+
+
+nome, idade = ("Bruna", 28)
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAM_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRAZILIAM_STATES)
---
+++ 04_convertendo_tipo.py
@@ -0,0 +1,13 @@
+print(int(1.80))
+print(int("10"))
+print(float("10.10"))
+print(float(100))
+
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(str(10.10)))
+
+
+print(100 / 2)
+print(100 // 2)
