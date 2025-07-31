804d0e940b235ad4307f424237f70ef8b10d9c6e
tassiocval <tassiocval@yahoo.com>
2025-07-31 17:43:02 -0300

    update

--- teste.py
+++ teste.py
@@ -1,9 +1,67 @@
-senha_correta = "admin"
-senha = input("Digite sua senha: ")
+# numero = int(input("Digite um número: "))
 
-while True:
-    senha = input("Digite sua senha: ")
+# if numero % 2 == 0:
+#     print(f"{numero} é Par")
+# else:
+#     print(f"{numero} é Impar")
 
-    if senha == senha_correta:
-        print("Login Válidado com sucesso!")
-        break
+# idade = int(input("Digite sua idade: "))
+
+# if idade >= 18:
+#     print("Maior de idade.")
+# else:
+#     print("Menor de idade.")
+
+# nota = float(input("Digite sua nota: "))
+
+# if nota >= 7:
+#     print("Aprovado.")
+# elif 4 <= nota <= 6:
+#     print("Recuperação")
+# else:
+#     print("Reprovado")
+
+# primeiro_numero = float(input("Digite um número: "))
+# segundo_numero = float(input("Digite mais um número: "))
+
+# if primeiro_numero > segundo_numero:
+#     print(f"{primeiro_numero} é maior que {segundo_numero}")
+# elif segundo_numero > primeiro_numero:
+#     print(f"{segundo_numero} é maior que {primeiro_numero}")
+# else:
+#     print("Os números são iguais")
+
+# temperatura = float(input("Ensira a temperatura atual em Celsius: "))
+
+# if temperatura < 15:
+#     print("Frio")
+# elif 15 <= temperatura <= 25:
+#     print("Agradável.")
+# elif temperatura > 25:
+#     print("Quente.")
+# else:
+#     print("Erro: temperatura invalida.")
+
+# numero = int(input("Insira um número de 1 a 7: "))
+
+# if numero == "1":
+#     semana = "Domingo."
+# elif numero == "2":
+#     semana = "Segunda-Feira."
+# elif numero == "3":
+#     semana = "Terça-Feira."
+# elif numero == "4":
+#     semana = "Quarta-Feira."
+# elif numero == "5":
+#     semana = "Quinta-Feira."
+# elif numero == "6":
+#     semana = "Sexta-Feira."
+# elif numero == "7":
+#     semana = "Sábado"
+# else:
+#     semana = "Escolha um valor válido." 
+
+# print(semana)
+
+frase = ["banana", "maçã", "uva"]
+print("-".join(frase))
